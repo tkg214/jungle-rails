0e2219093a03f9777d871cfffdef51d791643eb9
Ken <tkg214@gmail.com>
2017-03-16 14:40:56 +0000

    add unstyled review feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,26 @@
 class ReviewsController < ApplicationController
 
+  before_filter :authorize
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user_id = current_user[:id]
+    @review.save
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@product)
+  end
+
+  private
+  def review_params
+
+    params.require(:review).permit(:description, :rating)
+  end
+
 end
